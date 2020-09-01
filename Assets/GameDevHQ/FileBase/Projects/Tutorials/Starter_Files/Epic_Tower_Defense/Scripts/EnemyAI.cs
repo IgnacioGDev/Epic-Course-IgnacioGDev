e1702fca67bdeec7b946850b41dadde1a5925974
IgnacioGDev <ilagosm@hotmail.com>
2020-09-01 15:05:13 -0400

    NavMesh Surface / SpawnManager - Added

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/EnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _navMeshAgent;
+    [SerializeField]
+    private Transform _destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _destination = GameObject.Find("EndPoint").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(_destination.transform.position);
+        EnemyDestination(_destination.transform.position);
+    }
+
+    void EnemyDestination(Vector3 endPoint)
+    {
+        _navMeshAgent.SetDestination(endPoint);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Endpoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefabEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawner());
+    }
+
+    IEnumerator EnemySpawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            Instantiate(_prefabEnemy, transform.position, transform.rotation);
+        }
+        
+    }
+}
